4243878ea403e36219ff7906c236d38219650530
Nandhini2910 <68176822+Nandhini2910@users.noreply.github.com>
2022-08-02 17:48:35 +0530

    Delete SalesIncentiveBackend-main directory

--- SalesIncentiveBackend-main/src/main/java/com/example/SalesIncentiveBackend/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SalesIncentiveBackend.repository;
-
-import com.example.SalesIncentiveBackend.model.Product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository  extends JpaRepository<Product, Integer> {
-}
